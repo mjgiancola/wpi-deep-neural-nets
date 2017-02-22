a9a57d053a425fcfbc088ce2deb679e91c7bdf84
Mike Giancola <mjgiancola@wpi.edu>
2017-02-22 14:32:32 -0500

    Fixed accuracy function

--- assignment4/homework4_mjgiancola_tapetri.py
+++ assignment4/homework4_mjgiancola_tapetri.py
@@ -5,29 +5,21 @@ import sys
 import numpy as np
 from scipy.optimize import check_grad
 from sklearn.linear_model import LogisticRegression
-
-###################################
-############ TODO #################
-###################################
-# Update loss function
-# Delete code we don't need from hw3
+from sklearn.metrics import accuracy_score
 
 def accuracy(weights, data, labels):
 
     y_hats = soft_max(np.dot(digits, weights))
-    y_hats[np.where(y_hats==np.max(y_hats))] = 1
-    a[np.where(a==np.max(a))] = 1
-
-# Performs logistic sigmoid element-wise on matrix x
-def sigmoid_array(x):
-  return 1.0 / (1.0 + np.exp(-x))
+    y_actuals = labels
+    m = len(labels)
+    
+    return sum( np.argmax(y_hats, axis=1) == labels ) / m
 
 def soft_max(x):
     e = np.exp(x)
     result = e / e.sum(axis = 1)[:, None]
     return result
 
-# Original cost function (from Homework 2)
 def J (W, digits, labels):
 
     # W - 784 * 10
@@ -41,7 +33,6 @@ def J (W, digits, labels):
 
     return result
 
-# Original gradient (from Homework 2)
 def gradJ (W, digits, labels):
 
     m = digits.shape[0]
@@ -54,31 +45,22 @@ def gradJ (W, digits, labels):
 
     return result
 
-# cost and gradient are function pointers for the loss function and respective gradient
-# w is the starting weights
-# We parameterized learning_rate and tolerance because problems 2 and 3 respond better to different values
-# freq determines how often information will be printed (every freq iterations)
-def gradientDescent (trainingData, trainingLabels, cost, gradient, w, learning_rate, max_iter, freq):
+def gradientDescent (trainingData, trainingLabels, w, learning_rate, max_iter):
 
     # Initialize starting values
     lastJ = np.inf
-    currentJ = cost(w, trainingData, trainingLabels) 
+    currentJ = J(w, trainingData, trainingLabels) 
     delta = lastJ - currentJ
     num_iter = 1
     
     while (num_iter < max_iter):
-    
-        # Problem 2 runs for ~80 iterations
-        # Problem 3 runs for ~19000 iterations
-        # This allows us to show every iteration for Problem 2,
-        # while only showing every 100 iterations for Problem 3
-        if (not (num_iter % freq)):
-            print("%4d: J = %10f\t||w|| = %5f\tDelta = %5f" % ((num_iter, currentJ, np.linalg.norm(w), delta)))
+
+        print("%4d: J = %10f\t||w|| = %5f\tDelta = %5f" % ((num_iter, currentJ, np.linalg.norm(w), delta)))
         
         # Update values
         lastJ = currentJ
-        w = w - ( learning_rate * gradient(w, trainingData, trainingLabels) )
-        currentJ = cost(w, trainingData, trainingLabels)
+        w = w - ( learning_rate * gradJ(w, trainingData, trainingLabels) )
+        currentJ = J(w, trainingData, trainingLabels)
         delta = abs(lastJ - currentJ)
         num_iter += 1
         
@@ -92,15 +74,13 @@ if __name__ == "__main__":
     testingLabels = np.load("mnist_test_labels.npy")
     
 
-
-    print(trainingDigits.shape)
+    # print(trainingDigits.shape)
     print(trainingLabels.shape)
-    print(testingDigits.shape)
-    print(testingLabels.shape)
-
+    # print(testingDigits.shape)
+    # print(testingLabels.shape)
+    exit()
     # W = np.random.randn(784, 10) / 10
     W = np.zeros((784,10))
-    print(W)
 
     print("J")
     print(J(W, trainingDigits, trainingLabels))
@@ -108,7 +88,7 @@ if __name__ == "__main__":
     print("gradJ")
     print(gradJ(W, trainingDigits, trainingLabels))
 
-    w_result = gradientDescent(trainingDigits, trainingLabels, J, gradJ, W, 5, 200 ,1)
+    w_result = gradientDescent(trainingDigits, trainingLabels, W, 0.5, 200)
 
     exit()
 
