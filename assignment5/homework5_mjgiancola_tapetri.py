334ac5e2a48f6ac34160641b7e8c827ae05d1226
Mike Giancola <mjgiancola@wpi.edu>
2017-03-14 15:00:54 -0400

    Finished forward, started back prop

--- assignment5/homework5_mjgiancola_tapetri.py
+++ assignment5/homework5_mjgiancola_tapetri.py
@@ -3,10 +3,9 @@ import numpy as np
 from scipy.optimize import check_grad
 import matplotlib.pyplot as plt
 
-# TODO Update
 def accuracy(weights, digits, labels):
 
-  y_hats = soft_max(np.dot(digits, weights))
+  _, _, _, y_hats = feed_forward(digits, W1, W2, b1, b2)
   y_actuals = labels
   
   return np.mean(np.argmax(y_hats, axis=1) == np.argmax(y_actuals, axis=1))
@@ -23,9 +22,10 @@ def plot_weights_vectors(w):
 def relu(x):
   return np.maximum(x, 0)
 
-# TODO: Use numpy to optimize
 def relu_prime(x):
-  return [1 if elt > 0 else 0 for elt in x]
+  result = np.zeros(x.shape)
+  result[np.nonzero(x>=0)] = 1 # To satisfy check_grad, relu' is 1 at 0
+  return result
 
 def soft_max(x):
     row_max = np.max(x, axis = 1,keepdims=True)
@@ -33,136 +33,153 @@ def soft_max(x):
     e_x = np.exp(max_removed)
     return e_x / e_x.sum(axis = 1, keepdims=True) 
 
-return z1, h1, z2, y_hats
+def J (W1, W2, b1, b2, digits, labels):
 
-def J (W1, b1, W2, b2, digits, labels):
-
-  # W - 784 * 10
-  # digits - 55000 * 784
   m = digits.shape[0]
 
-  # 55000 * 10
-  y_hats = feed_forward(W1, b1, W2, b2, digits)
+  _, _, _, y_hats = feed_forward(digits, W1, W2, b1, b2)
   y_actuals = labels
 
   result = -1.0/m * np.sum(np.multiply(y_actuals, np.log(y_hats)))
 
   return result
 
-def feed_forward(batch, W1, b1, W2, b2):
+def feed_forward(batch, W1, W2, b1, b2):
   """
   Runs the given batch through the network defined by the given weights
   and return the intermediate values z1, h1, z2 as well as the final y_hats
   """
 
-  # digits is num_instances (varies) * 784
-  # W1 is 784 * 30
-  # z1 should be num_instances * 30
+  # batch is num_instances (varies) * 784
+  # W1 is 784 * num_hidden_units (varies)
+  # z1 (and h1) should be num_instances * num_hidden_units
 
-  # 55000 * 30
-  z1_no_bias = np.dot(digits, W1)
+  z1_no_bias = np.dot(batch, W1)
   z1 =  z1_no_bias + b1.T
   h1 = relu(z1)
 
-  # 55000 * 10 
+  # h1 is num_instances * num_hidden_units
+  # W2 is num_hidden_units * 10
+  # z2 (and y_hats) should be num_instances * 10
+
   z2_no_bias = np.dot(h1, W2)
   z2 = z2_no_bias + b2.T
   y_hats = soft_max(z2)
 
+  return z1, h1, z2, y_hats
+
 # TODO: db1, dW2, db2, change intermediate value names a, b
-def backprop(batch, batch_labels, z1, h1, z2, y_hats, W1, b1, W2, b2):
+def backprop(batch, batch_labels, z1, h1, z2, y_hats, W1, W2, b1, b2):
   """
   Runs backpropagation through the network and returns the gradients for 
   J with respect to W1, b1, W2, and b2.
   """
 
-  # 55000 * 10
-  a = np.dot((y_hats - batch_labels), W2)
+  y_actuals = batch_labels
 
-  z1 = np.dot(batch, W1) + b1
-  b = relu_prime(z1)
+  dJdz2 = (y_hats - y_actuals)
 
-  g = (a * b).T
+  ####### dW1 #######
+
+  dJdh1 = np.dot(dJdz2, W2)
+
+  g = (dJdh1 * relu_prime(z1)).T
 
   # Compute outer product
-  dW1 =  np.dot(g, batch.T)
+  dW1 = np.dot(g, batch.T)
 
-  return dW1, db1, dW2, db2
+  ###################
+
+  ####### dW2 #######
+
+  dW2 = np.dot(dJdz2, h1)
+
+  ###################
+
+  # TODO Fix
+  db1 = 0
+  db2 = 0
+
+  return dW1, dW2, db1, db2
   
 
 # TODO:
 def SGD (trainingData, trainingLabels, hidden_units, learn_rate, batch_size, num_epochs, reg_strength):
   """
-  Trains a 3-layer NN with the given hyper parameters and return the weights W1, b1, W2, b2 when done learning.
+  Trains a 3-layer NN with the given hyper parameters and return the weights W1, W2, b1, b2 when done learning.
   """
 
-    # Initialize weight vectors
-    (W1, W2, b1, b2) = initialize_weights(hidden_units)
+  # Initialize weight vectors
+  (W1, W2, b1, b2) = initialize_weights(hidden_units)
 
-    # Initialize starting values
-    lastJ = np.inf
-    currentJ = J(W1, b1, W2, b2, trainingDigits, trainingLabels)
-    delta = lastJ - currentJ
-    
-    for epoch in range(1, num_epochs):
+  # Initialize starting values
+  lastJ = np.inf
+  currentJ = J(W1, W2, b1, b2, trainingDigits, trainingLabels)
+  delta = lastJ - currentJ
+  
+  for epoch in range(1, num_epochs):
+    print 1
+    # Extract new batch from data / Loop over all batches once?
 
-      # Extract new batch from data / Loop over all batches once?
+    # Compute J using feedforward
 
-      # Compute J using feedforward
+    # Update weights using backprop values * learning rate
 
-      # Update weights using backprop values * learning rate
-
-
-    return W1, b1, W2, b2
+  return W1, W2, b1, b2
 
 
 def initialize_weights(hidden_units = 30):
+  """
+  Initializes weight and bias vectors
+  """
 
-    w1_abs = 1.0 / np.sqrt(784)
-    w2_abs = 1.0 / np.sqrt(30)
+  w1_abs = 1.0 / np.sqrt(784)
+  w2_abs = 1.0 / np.sqrt(30)
 
-    W_1 =  np.random.uniform(-w1_abs,w1_abs,[784, hidden_units]) # 784 x hidden_units
-    W_2 = np.random.uniform(-w2_abs,w2_abs,[hidden_units, 10]) # hidden_units x 10
-    b_1 = 0.01 * np.ones((hidden_units,1)) # hidden_units x 1
-    b_2 = 0.01 * np.ones((10,1)) # 10 x 1
+  W_1 =  np.random.uniform(-w1_abs,w1_abs,[784, hidden_units]) # 784 x hidden_units
+  W_2 = np.random.uniform(-w2_abs,w2_abs,[hidden_units, 10]) # hidden_units x 10
+  b_1 = 0.01 * np.ones((hidden_units,1)) # hidden_units x 1
+  b_2 = 0.01 * np.ones((10,1)) # 10 x 1
 
-    return W_1, W_2, b_1, b_2
+  return W_1, W_2, b_1, b_2
 
 if __name__ == "__main__":
    
-    # Load data
-    trainingDigits = np.load("mnist_train_images.npy")
-    trainingLabels = np.load("mnist_train_labels.npy")
-    validationDigits = np.load("mnist_validation_images.npy")
-    validationLabels = np.load("mnist_validation_labels.npy")
-    testingDigits = np.load("mnist_test_images.npy")
-    testingLabels = np.load("mnist_test_labels.npy")
-    
+  # Load data
+  trainingDigits = np.load("mnist_train_images.npy")
+  trainingLabels = np.load("mnist_train_labels.npy")
+  validationDigits = np.load("mnist_validation_images.npy")
+  validationLabels = np.load("mnist_validation_labels.npy")
+  testingDigits = np.load("mnist_test_images.npy")
+  testingLabels = np.load("mnist_test_labels.npy")
 
-    # Hyper parameters
-    num_hidden_units = [30,40,50]
-    learning_rate = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
-    minibatch_size = [16, 32, 64, 128, 256]
-    num_epochs = [100,1000,10000] # ?
-    reg_strength = [0.01, 0.1, 0.2, 0.5]
+  # Hyper parameters
+  num_hidden_units = [30,40,50]
+  learning_rate = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+  minibatch_size = [16, 32, 64, 128, 256]
+  num_epochs = [100,1000,10000] # ?
+  reg_strength = [0.01, 0.1, 0.2, 0.5]
 
-    # For each set of hyper param settings
-    #    * train NN on training set
-    #    * validate NN on validation set
-    # Report final accuracy on test set
+  # For each set of hyper param settings
+  #    * train NN on training set
+  #    * validate NN on validation set
+  # Report final accuracy on test set
 
-    hidden_units = 30 # default for now
+  # Initialize weight vectors
+  (W_1, W_2, b_1, b_2) = initialize_weights()
 
-    # Initialize weight vectors
-    (W_1, W_2, b_1, b_2) = initialize_weights(hidden_units)
+  print("Initial cost for initialized weights, J = " + str(J(W_1, W_2, b_1, b_2, trainingDigits, trainingLabels)))
 
-    print("Initial cost for initialized weights, J = " + str(J(W_1, b_1, W_2, b_2, trainingDigits, trainingLabels)))
+  # TODO Use check_grad to confirm gradient functions work
 
-    # Run gradient descent with learning_rate=0.5, num_iter=325
-    # W = gradientDescent(trainingDigits, trainingLabels, W, 0.5, 325)
-    
-    # print "Loss on Test Set: " + str(J(W, testingDigits, testingLabels))
-    # print "Accuracy on Test Set: " + str(accuracy(W, testingDigits, testingLabels))
+  # NOTE: NEW ACCURACY FUNCTION
+  # print accuracy(testingDigits, testingLabels, W_1, W_2, b_1, b_2)
 
-    # plot_weights_vectors(W)
+  # Run gradient descent with learning_rate=0.5, num_iter=325
+  # W = gradientDescent(trainingDigits, trainingLabels, W, 0.5, 325)
+  
+  # print "Loss on Test Set: " + str(J(W, testingDigits, testingLabels))
+  # print "Accuracy on Test Set: " + str(accuracy(W, testingDigits, testingLabels))
+
+  # plot_weights_vectors(W)
 
